50a61d6d1724c65c0c4eb9bd1e45a95ca33b1926
Mateusz Sędkowski <mateusz.sedkowski@htdevelopers.com>
2019-08-17 17:23:36 +0200

    Add Formik into project and initialize it in sign up page

--- src/components/Authorization/Login/SignUp/SignUp.js
+++ src/components/Authorization/Login/SignUp/SignUp.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './SignUp.css'
 import {Auth} from "aws-amplify"
-import {Link} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
 
 class SignUp extends Component {
     state = {
-        firstName: '',
-        lastName: '',
-        username: '',
-        phone: null,
-        password: '',
-        gender: 'Female',
-        birthdate: '17-04-1993',
-        address: 'Rojna 50c/80',
+        user: {
+            first_name: '',
+            last_name: '',
+            email: '',
+            phone: null,
+            password: '',
+            gender: 'Female',
+            birthdate: '17-04-1993',
+            address: 'Rojna 50c/80',
+        },
         isLoading: false
     }
 
@@ -22,52 +25,104 @@ class SignUp extends Component {
         })
     }
 
-    signUpHandler = async event => {
-        event.preventDefault();
-
+    signUpHandler = async values => {
         this.setState({isLoading: true});
-
-        try {
-            await Auth.signUp({
-                username: this.state.username,
-                password: this.state.password,
-                attributes: {
-                    email: this.state.username,
-                    phone_number: this.state.phone,
-                    name: this.state.firstName,
-                    given_name: this.state.lastName,
-                    gender: this.state.gender,
-                    birthdate: this.state.birthdate,
-                    address: this.state.address
-                }
-            })
-        } catch (e) {
-            console.log(e.message); //TODO REMOVE AT END
+        let user = this.state.user
+        await Auth.signUp({
+            username: values.email,
+            password: values.password,
+            attributes: {
+                email: values.email,
+                phone_number: values.phone,
+                name: values.first_name,
+                given_name: values.last_name,
+                gender: values.gender,
+                birthdate: user.birthdate,
+                address: user.address
+            }
+        }).then(_ => {
             this.setState({isLoading: false});
-        }
+            console.log("Done")
+            this.props.history.push('/')
+        }, rejected => {
+            console.log("Rejected", rejected); //TODO REMOVE AT END
+        })
     }
 
-    render () {
+    render() {
         return (
             <div className="SignUp">
                 <div className="Controls">
                     <h1> Create Account </h1>
-                    <label>First Name: </label>
-                    <input name='firstName' onChange={this.handleChange}/>
-                    <label>Last Name: </label>
-                    <input name='lastName' onChange={this.handleChange}/>
-                    <label>Email: </label>
-                    <input type='email' name='username' onChange={this.handleChange}/>
-                    <label>Phone: </label>
-                    <input name='phone' onChange={this.handleChange}/>
-                    <label>Password: </label>
-                    <input type='password' name='password' onChange={this.handleChange}/>
-                    <div className="FormActions">
-                        <Link
-                            className="LogIn"
-                            to='/'>Already have an account?</Link>
-                        <button onClick={this.signUpHandler}>Sign Up</button>
-                    </div>
+                    <Formik
+                        initialValues={
+                            {
+                                first_name: '',
+                                last_name: '',
+                                email: '',
+                                gender: null,
+                                phone: '',
+                                password: ''
+                            }
+                        }
+                        validate={values => {
+                            let errors = {};
+                            if (!values.email) {
+                                errors.email = 'This field is required.';
+                            } else if (
+                                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                            ) {
+                                errors.email = 'Invalid email address.';
+                            }
+                            return errors;
+                        }}
+                        onSubmit={(values) => this.signUpHandler(values)}
+                    >
+                        {({isSubmitting}) => (
+                            <Form>
+                                <div className='form-group'>
+                                    <label>First Name </label>
+                                    <Field type='text' name="first_name"/>
+                                    <ErrorMessage name="first_name" component="div" className='errorMessage'/>
+                                </div>
+                                <div className='form-group'>
+                                    <label>Last Name </label>
+                                    <Field type='text' name="last_name"/>
+                                    <ErrorMessage name="last_name" component="div" className='errorMessage'/>
+                                </div>
+                                <div className='form-group'>
+                                    <label>Email </label>
+                                    <Field type='email' name="email"/>
+                                    <ErrorMessage name="email" component="div" className='errorMessage'/>
+                                </div>
+                                <div className='form-group'>
+                                    <label>Phone </label>
+                                    <Field type='text' name="phone"/>
+                                    <ErrorMessage name="phone" component="div" className='errorMessage'/>
+                                </div>
+                                <div className='form-group'>
+                                    <label>Gender </label>
+                                    <Field component="select" name="gender" className='genderSelect'>
+                                        <option value="men">Men</option>
+                                        <option value="women">Women</option>
+                                    </Field>
+                                </div>
+                                <div className='form-group'>
+                                    <label>Password </label>
+                                    <Field type='password' name="password"/>
+                                    <ErrorMessage name="password" component="div" className='errorMessage'/>
+                                </div>
+                                <div className="FormActions">
+                                    <Link
+                                        className="LogIn"
+                                        to='/'>Already have an account?</Link>
+                                    <button type="submit" disabled={isSubmitting}>
+                                        Sign Up
+                                    </button>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
                 </div>
             </div>
         )
